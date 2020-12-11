4d6414ca0144e846f909925484d2db8b2c5e637e
tuvetula <romain.marcant@gmail.com>
2020-12-12 00:09:29 +0100

    modify recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'preparation_time',
+        'baking_time',
+        'author_comment',
+        'picture'
+    ];
 }
