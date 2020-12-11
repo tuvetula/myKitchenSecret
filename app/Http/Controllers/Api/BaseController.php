54fc754bddd3f4144f9881199a2957b3977c0884
tuvetula <romain.marcant@gmail.com>
2020-12-12 00:49:25 +0100

    create BaseController

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @param $result
+     * @param $message
+     * @return JsonResponse
+     */
+    public function sendResponse($result, $message): JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * return error response.
+     *
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function sendError($error, $errorMessages = [], $code = 404): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
