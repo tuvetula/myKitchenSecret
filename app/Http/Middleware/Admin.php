f4ef025f3a546ebf8425c39e9c1fba154a9c7495
tuvetula <romain.marcant@gmail.com>
2020-12-12 02:26:59 +0100

    modify qualifier

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -14,7 +14,7 @@ class Admin
      * Handle an incoming request.
      *
      * @param Request $request
-     * @param  \Closure  $next
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
