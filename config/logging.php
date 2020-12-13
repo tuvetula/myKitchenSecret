53dfcd5d1bc8da0dafd606c6dcbc10b100b1ee82
tuvetula <romain.marcant@gmail.com>
2020-12-13 01:45:52 +0100

    add recipe loggin

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
@@ -50,6 +50,12 @@ return [
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+        'recipe' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/Recipe/recipe.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
