c71d94cce91e45908dfb21925d6407c60a44a879
tuvetula <romain.marcant@gmail.com>
2020-12-19 15:30:40 +0100

    add Login and Register FormRequest

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'first_name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+            'password_confirmation' => 'required_with:password|string|min:8|same:password',
+            'is_admin' => 'boolean'
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this['is_admin'] = false;
+    }
+}
